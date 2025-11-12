e149b45ed3d9943cbd3beced0583281decb3c500
2400032ChiaChi <chiachitsai@systex.com>
2025-11-12 14:51:03 +0800

    Github Pages部版設定

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   /* config options here */
+//   reactCompiler: true,
+// };
+
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  output: 'export', 
+  basePath: '/markdown-preview-nextjs', 
+  images: {
+    unoptimized: true, 
+  },
 };
 
 export default nextConfig;
