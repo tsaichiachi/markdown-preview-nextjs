5cd0671aee2214553570968f139a31a3a78ce5cd
2400032ChiaChi <chiachitsai@systex.com>
2025-11-12 15:24:42 +0800

    fix asset prefix

--- _next/static/vkWYXlYmpsuqeLG79uoCl/_buildManifest.js
+++ _next/static/vkWYXlYmpsuqeLG79uoCl/_buildManifest.js
@@ -1,7 +1,12 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/markdown-preview-nextjs//_next/:path+",
+        "destination": "/_next/:path+"
+      }
+    ],
     "fallback": []
   },
   "sortedPages": [
